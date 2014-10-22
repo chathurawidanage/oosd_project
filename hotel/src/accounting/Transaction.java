b450d966262a0990cdf68f38dc5ad0d758552cf6
mdslasantha <Lasantha@10.10.42.36>
2014-10-22 16:10:40 +0530

    Recreated

--- hotel/src/accounting/Transaction.java
+++ hotel/src/accounting/Transaction.java
@@ -1,6 +1,8 @@
 package accounting;
 
-import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import utilities.DataBase;
@@ -10,32 +12,51 @@ import utilities.DataBase;
  * @author Lasantha Ekanayake <lasantha.12@cse.mrt.ac.lk>
  */
 public class Transaction {
+
     private double amount = 0.00;
     private Date date = null;
-    private String details = "";    // ex: "Bill Payment" , "Cash Recieved from xxxxx"
-    private boolean type = false;   // true for credit and false for debit transactions
+    private String details = "";    // ex: "Bill Payment" , "Cash Recieved"
+    private int type = 0;   // type>0 for credit and type<0 for debit transactions
     private int id = 0; // unique for each transactions
-    
+
     // constructors
-    public Transaction(Date date, double amount, String details, boolean type){
+    public Transaction(Date date, double amount, String details, int type) {
         this.date = date;
         this.amount = amount;
         this.details = details;
         this.type = type;
     }
-    
+
+    public Transaction(int id) throws SQLException {
+        ArrayList<String> col = new ArrayList<>();
+        col.add("date");
+        col.add("amount");
+        col.add("details");
+        col.add("type");
+
+        ResultSet results = DataBase.select("transactions", col, "id = " + id);
+
+        if (results.next()) {
+            this.date = results.getDate("date");
+            this.amount = results.getDouble("amount");
+            this.details = results.getString("details");
+            this.type = results.getInt("type");
+        }
+
+    }
+
     // save in the database
-    public int Save(){
-        HashMap<String,Object> data = new HashMap<>();
-        
+    public int Save() {
+        HashMap<String, Object> data = new HashMap<>();
+
         data.put("date", this.date);
         data.put("amount", this.amount);
         data.put("details", this.details);
         data.put("type", this.type);
-        
+
         this.id = DataBase.insert("transactions", "id", data);
-        
+
         return this.id;
     }
-    
+
 }
