89ac157bfa1010f8b97ba7276becee4ed95c75eb
mdslasantha <Lasantha@10.10.42.36>
2014-10-22 10:10:08 +0530

    Transaction class created. No get set yet.

--- hotel/src/accounting/Transaction.java
+++ hotel/src/accounting/Transaction.java
@@ -8,11 +8,21 @@ import java.util.Date;
  * @author Lasantha Ekanayake <lasantha.12@cse.mrt.ac.lk>
  */
 public class Transaction {
-    private BigDecimal amount = null;
+    private double amount = 0.00;
     private Date date = null;
+    private String details = "";    // ex: "Bill Payment" , "Cash Recieved from xxxxx"
+    private boolean type = false;   // true for credit and false for debit transactions
+    private int id = 0; // unique for each transactions
     
-    
-    public Transaction(){
-        
+    // constructors
+    public Transaction(Integer id, Date date, double amount, String details){
+        this.id = id;
+        this.date = date;
+        this.amount = amount;
+        this.details = details;
+        this.type = amount>0;
     }
+    
+    
+    
 }
