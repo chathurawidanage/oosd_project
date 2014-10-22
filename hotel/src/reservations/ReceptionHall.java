f8bef6465bfc48241e3bc06da522dd857361ff65
aseladarshan <aseladarshan@asela-dell-3521>
2014-10-22 17:26:24 +0530

    methods implemented

--- hotel/src/reservations/ReceptionHall.java
+++ hotel/src/reservations/ReceptionHall.java
@@ -12,4 +12,36 @@ package reservations;
  */
 public class ReceptionHall {
     private String name;
+    private int id;
+    public ReceptionHall(String name,int id){
+        this.name=name;
+        this.id=id;
+    }
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- hotel/src/reservations/Reservation.java
+++ hotel/src/reservations/Reservation.java
@@ -6,21 +6,121 @@
 
 package reservations;
 import elements.Customer;
-import java.sql.Date;
-//import java.util.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import utilities.DataBase;
 //import sun.util.calendar.LocalGregorianCalendar.Date;
 /**
  *
  * @author aseladarshan
  */
 public class Reservation {
-    private ReceptionHall[] receptionHall;
-    public boolean reserve(Customer customer){
-        
-        return false;
+    private final int NoOfHalls;
+    private final ReceptionHall[] receptionHall;
+    private ArrayList<String> dbCols;
+    private final String dbTable;
+    public Reservation(){
+        dbCols.add("Date");
+        dbCols.add("hall");
+        dbTable="reservations";
+        manageHalls mh=new manageHalls();
+        receptionHall=manageHalls.getReceptionHall();
+        NoOfHalls=manageHalls.getNoOfHalls();
+            
     }
-    public boolean checkAvaliability(Date date){
-        
-        return false;
+    
+        /**
+     *
+     * @param customer
+     * @param hall 
+     * @param date reserving date
+     * @return return true if reservation successful ,false if unsuccess
+     */
+    public boolean reserve(Customer customer,ReceptionHall hall,Date date){
+        HashMap map=new HashMap();
+        int hallID=hall.getId();
+        String customerID=customer.getId();
+        int insert;
+        map.put(date,hallID);
+        insert = DataBase.insert(dbTable,"Date",map);
+        map.put(insert,customerID);
+        DataBase.insert(dbTable,"CustomerID",map);
+        return true;
     }
+    
+        /**
+     *
+     * @param customer
+     * @param hall
+     * @param from starting date of range
+     * @param to ending date of range
+     * @return return true if reservation successful ,false if unsuccess
+     */
+     public boolean reserve(Customer customer,ReceptionHall hall,Date from,Date to){
+         Date reserveDate=from;
+         while(reserveDate.before(to))
+        {
+            if(!(reserve(customer,hall,reserveDate)))
+                return false;
+            reserveDate.setTime(reserveDate.getTime()+(24*60*60*1000));
+        }
+         return true;
+     }
+         /**
+     *
+     * @param  date date which is need to check
+     * @return arrayList of avaliable halls
+     */
+    public ArrayList<Integer> checkAvaliability(Date date){
+        ArrayList<Integer> halls=new ArrayList<>();
+        for(int i=1;i<=NoOfHalls;i++){
+            halls.add(i);
+        }
+        ResultSet select;
+        try {
+            select= DataBase.select(dbTable, dbCols, "Date='" + date.toString() + "'");
+            int[] array = (int[]) select.getArray("hall").getArray();
+            for(int i=0;i<=array.length;i++){
+                halls.remove(array[i]);
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Reservation.class.getName()).log(Level.SEVERE, null, ex);
+        }     
+        return halls;
+    }
+        /**
+     *
+     * @param  from starting date of the range
+     * @param  to ending date of the range 
+     * @return arrayList of avaliable halls for all days in the range
+     */
+    public  ArrayList<Integer> checkAvaliability(Date from,Date to){
+        Date checkDate=from;
+        int[] hallArry=new int[NoOfHalls];
+        ArrayList<Integer> halls=new ArrayList<>();
+        int days=0;
+        while(checkDate.before(to))
+        {
+            ArrayList<Integer> list = this.checkAvaliability(checkDate);
+            for (Integer list1 : list) {
+                hallArry[list1]++;
+            }
+            checkDate.setTime(checkDate.getTime()+(24*60*60*1000));
+            days++;
+        }
+        for(int i=0;i<NoOfHalls;i++){
+            if(hallArry[i]==days){
+                halls.add(hallArry[i]);
+            }
+        }
+        return halls;
+    }
+
+    
 }
