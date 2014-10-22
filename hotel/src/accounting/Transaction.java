e1afda1a2bacee4f7b896eefa9bb1937a430a111
Chathura <Chathura@Chathura-Lap>
2014-10-22 09:31:41 +0530

    initialized

---
+++ hotel/src/accounting/Transaction.java
@@ -0,0 +1,18 @@
+package accounting;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ *
+ * @author Lasantha Ekanayake <lasantha.12@cse.mrt.ac.lk>
+ */
+public class Transaction {
+    private BigDecimal amount = null;
+    private Date date = null;
+    
+    
+    public Transaction(){
+        
+    }
+}
